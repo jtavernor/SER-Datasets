ed32c822d1ef807020f02d6171df5709cf470422
James Tavernor <tavernor@umich.edu>
2024-06-21 16:04:22 -0400

    Raise error if config does not exist

--- src/SERDatasets/config.py
+++ src/SERDatasets/config.py
@@ -13,6 +13,7 @@ class Config:
                     conf = pyyaml.safe_load(config_file)
                 with open(config_path, 'w') as config_file:
                     config_file.write(pyyaml.dump(conf))
+                raise IOError('Data config file did not exist. Created in current working directory, please confirm config is correct and re-run.')
             with open(config_path, 'r') as config_file:
                 cls.instance.config_file = pyyaml.safe_load(config_file)
         return cls.instance.config_file # Returns a shared dictionary
