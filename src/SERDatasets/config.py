ce5c6b990cfba657a16e3eab302510563c4531fe
James Tavernor <tavernor@umich.edu>
2024-06-21 16:01:24 -0400

    Use a run-directory config instead of one inside the venv

--- src/SERDatasets/config.py
+++ src/SERDatasets/config.py
@@ -7,7 +7,12 @@ class Config:
             cls.instance = super(Config, cls).__new__(cls)
             dir_path = os.path.dirname(os.path.realpath(__file__))
             run_location = os.getcwd()
-            config_path = os.path.join(dir_path, 'data_config.yaml')
+            config_path = os.path.join(run_location, 'data_config.yaml')
+            if not os.path.exists(config_path):
+                with open(os.path.join(dir_path, 'data_config.yaml'), 'r') as config_file:
+                    conf = pyyaml.safe_load(config_file)
+                with open(config_path, 'w') as config_file:
+                    config_file.write(pyyaml.dump(conf))
             with open(config_path, 'r') as config_file:
                 cls.instance.config_file = pyyaml.safe_load(config_file)
         return cls.instance.config_file # Returns a shared dictionary
