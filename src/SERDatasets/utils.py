970978e569efcd6aac1e21b306924cb9c5b5d490
James Tavernor <tavernor@umich.edu>
2025-03-26 11:23:59 -0400

    Fix add muse annotators

--- src/SERDatasets/utils.py
+++ src/SERDatasets/utils.py
@@ -91,20 +91,22 @@ def add_muse_annotators_NO_SELF_REPORT(train_dataset, val_dataset, test_dataset)
     muse_annotator_info_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'muse_annotators.csv')
     muse_ann_df = pd.read_csv(muse_annotator_info_path)
     muse_ann_df = muse_ann_df.set_index('FileName')
-    muse_ann_df.soft_act_labels = muse_ann_df.soft_act_labels.apply(lambda s: [-1+(float(x.strip(' []'))-1)*2/(9-1) for x in s.split(',')])
-    muse_ann_df.soft_val_labels = muse_ann_df.soft_val_labels.apply(lambda s: [-1+(float(x.strip(' []'))-1)*2/(9-1) for x in s.split(',')])
+    muse_ann_df.soft_act_labels = muse_ann_df.soft_act_labels.apply(lambda s: [float(x.strip(' []')) for x in s.split(',')])
+    muse_ann_df.soft_val_labels = muse_ann_df.soft_val_labels.apply(lambda s: [float(x.strip(' []')) for x in s.split(',')])
     muse_ann_df.annotators = muse_ann_df.annotators.apply(lambda s: [x.strip(' []\'') for x in s.split(',')])
     def convert_dataset(sample):
         new_values = muse_ann_df.loc[sample['FileName']].to_dict()
-        new_values['soft_act_labels'] = new_values['soft_act_labels'][:-1]
-        new_values['soft_val_labels'] = new_values['soft_val_labels'][:-1]
-        new_values['annotators'] = new_values['annotators'][:-1]
-        return {**sample, **new_values, 'act': np.mean(new_values['soft_act_labels']), 'val': np.mean(new_values['soft_val_labels'])}
+        sample['soft_act_labels'] = new_values['soft_act_labels'][:-1]
+        sample['soft_val_labels'] = new_values['soft_val_labels'][:-1]
+        sample['annotators'] = new_values['annotators'][:-1]
+        sample['act'] = np.mean(sample['soft_act_labels'])
+        sample['val'] = np.mean(sample['soft_val_labels'])
+        return sample
 
-    train_dataset = train_dataset.map(convert_dataset)
-    val_dataset = val_dataset.map(convert_dataset)
-    test_dataset = test_dataset.map(convert_dataset)
-    all_ann = [a for ann in train_dataset['annotators'] + val_dataset['annotators'] + test_dataset['annotators'] for a in ann]
+    train_dataset = train_dataset.apply(convert_dataset, axis=1)
+    val_dataset = val_dataset.apply(convert_dataset, axis=1)
+    test_dataset = test_dataset.apply(convert_dataset, axis=1)
+    all_ann = [a for ann in train_dataset['annotators'].to_list() + val_dataset['annotators'].to_list() + test_dataset['annotators'].to_list() for a in ann]
     if any([a.startswith('self-report') for a in all_ann]):
         raise ValueError('Failed to remove self report')
 
